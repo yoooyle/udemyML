02eb00f28ef109c489b8bc67057cea956ad43d4d
Ye Yuan <yuanye@yuanye.org>
2017-11-07 09:21:03 -0800

    encode categorical column with no order

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -12,6 +12,14 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                         dataset$Salary)
 
+# Encoding categorical data. 
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased, 
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -8,13 +8,27 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 3].values 
 
 # Take care of missing values.
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy= 'mean', axis= 0)
 imputer = imputer.fit(X[:,1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+# Can't just do this. It will think one country is greater than others. 
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+# This way, the ML algorithm won't attribute order to the categorical feature.
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+# for the dependent var, the ML algorithem knows it is categorical.
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
