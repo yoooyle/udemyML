faa48a1fb57633318240a43c2e86ce1dfdcca2be
Ye Yuan <yuanye@yuanye.org>
2017-11-07 09:36:53 -0800

    split into training and test set

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -24,7 +24,7 @@ dataset$Purchased = factor(dataset$Purchased,
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
