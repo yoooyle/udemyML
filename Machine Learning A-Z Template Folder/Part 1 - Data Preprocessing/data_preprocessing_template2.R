0af3e4edfc90abb3add89f65818949798afcbdb3
Ye Yuan <yuanye@yuanye.org>
2017-11-07 21:35:25 -0800

    simple linear regression

---
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template2.R
@@ -0,0 +1,13 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+dataset = dataset[,2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template2.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template2.py
@@ -8,27 +8,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values 
-
-# Take care of missing values.
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy= 'mean', axis= 0)
-imputer = imputer.fit(X[:,1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-# Can't just do this. It will think one country is greater than others. 
-X[:,0] = labelencoder_X.fit_transform(X[:,0])
-# This way, the ML algorithm won't attribute order to the categorical feature.
-# If this is tshirt size S, M, L, then we should not do one hot.
-# AKA dummy variable.
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-
-# for the dependent var, the ML algorithem knows it is categorical.
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+y = dataset.iloc[:, -1].values 
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -45,7 +25,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 # Normalization
 # x-min(x)/max(x)-min(x)
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 # We are also scaling the dummy vars. 
 # If we scale, everything will be on the same scale. 
@@ -54,3 +34,4 @@ X_train = sc_X.fit_transform(X_train)
 # No fit on test set. 
 X_test = sc_X.transform(X_test)
 # No need to fit Y set because this is a classification problem.\
+"""
