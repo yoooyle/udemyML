232e9790026a9da6811d06b15a9d1c90186530fd
Ye Yuan <yuanye@yuanye.org>
2017-11-15 10:01:30 -0800

    decision tree, random forest and logit regression

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
@@ -1,4 +1,4 @@
-# Decision Tree Regression
+# Random Forest Regression
 
 # Importing the libraries
 import numpy as np
@@ -22,23 +22,20 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Decision Tree Regression to the dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state =     0)
+# Fitting Random Forest Regression to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
 regressor.fit(X, y)
 
 # Predicting a new result
 y_pred = regressor.predict(6.5)
 
-# Depending on the information entropy gains, it is split into
-# discrete intervals.
-# This could be a lot interesting in more Dimensions.
-# Visualising the Decision Tree Regressißn results (higher resolution)
+# Visualising the Random Forest Regression results (higher resolution)
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.title('Truth or Bluff (Random Forest Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/regression_template.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/regression_template.py
@@ -1,4 +1,4 @@
-# Decision Tree Regression
+# Regression Template
 
 # Importing the libraries
 import numpy as np
@@ -22,23 +22,26 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Decision Tree Regression to the dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state =     0)
-regressor.fit(X, y)
+# Fitting the Regression Model to the dataset
+# Create your regressor here
 
 # Predicting a new result
 y_pred = regressor.predict(6.5)
 
-# Depending on the information entropy gains, it is split into
-# discrete intervals.
-# This could be a lot interesting in more Dimensions.
-# Visualising the Decision Tree Regressißn results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
+# Visualising the Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
